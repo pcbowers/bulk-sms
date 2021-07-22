45c7546f567403819acb98812369fb288190ed75
P. Christopher Bowers <pcbowers@gmail.com>
2021-07-21 20:33:54 -0400

    updated middleware

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,24 +1,31 @@
+import { pipe } from "ramda"
 import {
   connectToDatabase,
   MAX_DB_OPERATIONS,
   paginate,
-  withCookies
+  withCookies,
+  withMethod,
+  withUserAuthentication
 } from "../../lib/export"
 
 async function handler(req, res) {
-  const {
-    cursor = "",
-    tags = "",
-    tag = "",
-    limit = MAX_DB_OPERATIONS.value
-  } = req.query
   // console.log(req.cookies)
   // const count = req.cookies.test === undefined ? 0 : parseInt(req.cookies.test) + 1
   // res.cookie("test", count)
 
-  console.log(MAX_DB_OPERATIONS.value)
   try {
+    res.limitMethods(["GET"])
+    await res.authenticateUser()
+
+    const {
+      cursor = "",
+      tags = "",
+      tag = "",
+      limit = MAX_DB_OPERATIONS.value
+    } = req.query
+
     await connectToDatabase()
+
     res
       .status(200)
       .json(
@@ -33,4 +40,4 @@ async function handler(req, res) {
   }
 }
 
-export default withCookies(handler)
+export default pipe(withCookies, withUserAuthentication, withMethod)(handler)
